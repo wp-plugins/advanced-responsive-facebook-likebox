9e6f49bd1f72a831e788dba5028d0dccdaa4d1b4
chrisravenwp <chrisravenwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-24 17:46:31 +0000

    stable release 1.0
    git-svn-id: https://plugins.svn.wordpress.org/advanced-responsive-facebook-likebox/trunk@971804 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Responsivefacebook.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * @package responsive-facebook-likebox
+*/
+/*
+Plugin Name: Advanced responsive facebook likebox
+Plugin URI: http://www.cr8tech.net
+Description: Responsive + Highly Customizable Facebook features.
+Version: 1.0
+Author: Chris Raven
+Author URI: http://www.cr8tech.net
+*/
+
+class Responsivefacebook extends WP_Widget{
+    
+    public function __construct() {
+        $params = array(
+            'description' => 'Thanks for installing Advanced responsive facebook likebox',
+            'name' => 'Advanced responsive facebook likebox'
+        );
+        parent::__construct('Responsivefacebook','',$params);
+    }
+    
+    public function form($instance) {
+        extract($instance);
+        
+        ?>
+        
+        <!-- Color Picker Script Start -->
+<script type="text/javascript">
+//<![CDATA[
+    jQuery(document).ready(function()
+    {
+	// colorpicker field
+	jQuery('.cw-color-picker').each(function(){
+	var $this = jQuery(this),
+        id = $this.attr('rel');
+ 	$this.farbtastic('#' + id);
+    });
+});		
+//]]>   
+</script>
+
+<!-- Color Picker Script End -->
+
+<!-- here will put all widget configuration -->
+		<p>
+			<label for="<?php echo $this->get_field_id('title');?>">Title : </label>
+			<input
+			class="widefat"
+			id="<?php echo $this->get_field_id('title');?>"
+			name="<?php echo $this->get_field_name('title');?>"
+				value="<?php echo !empty($title) ? $title : "Advanced responsive facebook likebox"; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('fb_url');?>">Facebook Page URL : </label>
+			<input
+			class="widefat"
+			id="<?php echo $this->get_field_id('fb_url');?>"
+			name="<?php echo $this->get_field_name('fb_url');?>"
+				value="<?php echo !empty($fb_url) ? $fb_url : "https://www.facebook.com/FacebookDevelopers"; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('height');?>">Height : </label>
+			<input
+			class="widefat"
+			id="<?php echo $this->get_field_id('height');?>"
+			name="<?php echo $this->get_field_name('height');?>"
+				value="<?php echo !empty($height) ? $height : "450"; ?>" />
+		</p>
+
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('backgroundColor');?>">Background Color : </label>
+			<input
+			class="widefat"
+			id="<?php echo $this->get_field_id('backgroundColor');?>"
+			name="<?php echo $this->get_field_name('backgroundColor');?>"
+				value="<?php echo !empty($backgroundColor) ? $backgroundColor : "#ffffff"; ?>" />
+		</p>
+		<div class="cw-color-picker backgroundColorHide" rel="<?php echo $this->get_field_id('backgroundColor'); ?>"></div>
+        
+        <p>
+			<label for="<?php echo $this->get_field_id('radius');?>">Border Radius : </label>
+			<input
+			class="widefat"
+			id="<?php echo $this->get_field_id('radius');?>"
+			name="<?php echo $this->get_field_name('radius');?>"
+				value="<?php echo !empty($radius) ? $radius : "5"; ?>" />
+		</p>
+        
+        <p>
+			<label for="<?php echo $this->get_field_id( 'color_scheme' ); ?>">Color Scheme:</label> 
+			<select id="<?php echo $this->get_field_id( 'color_scheme' ); ?>"
+				name="<?php echo $this->get_field_name( 'color_scheme' ); ?>"
+				class="widefat" style="width:100%;">
+					<option value="0" <?php if ($color_scheme == '0') echo 'selected="0"'; ?> >Light</option>
+					<option value="1" <?php if ($color_scheme == '1') echo 'selected="1"'; ?> >Dark</option>	
+			</select>
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'header' ); ?>">Show Header:</label> 
+			<select id="<?php echo $this->get_field_id( 'header' ); ?>"
+				name="<?php echo $this->get_field_name( 'header' ); ?>"
+				class="widefat" style="width:100%;">
+					<option value="true" <?php if ($header == 'true') echo 'selected="true"'; ?> >Yes</option>
+					<option value="false" <?php if ($header == 'false') echo 'selected="false"'; ?> >No</option>	
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'face' ); ?>">Show Face:</label> 
+			<select id="<?php echo $this->get_field_id( 'face' ); ?>"
+				name="<?php echo $this->get_field_name( 'face' ); ?>"
+				class="widefat" style="width:100%;">
+					<option value="true" <?php if ($face == 'true') echo 'selected="true"'; ?> >Yes</option>
+					<option value="false" <?php if ($face == 'false') echo 'selected="false"'; ?> >No</option>	
+			</select>
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'border' ); ?>">Show Border:</label> 
+			<select id="<?php echo $this->get_field_id( 'border' ); ?>"
+				name="<?php echo $this->get_field_name( 'border' ); ?>"
+				class="widefat" style="width:100%;">
+					<option value="true" <?php if ($border == 'true') echo 'selected="true"'; ?> >Yes</option>
+					<option value="false" <?php if ($border == 'false') echo 'selected="false"'; ?> >No</option>	
+			</select>
+		</p>
+        
+        <p>
+			<label for="<?php echo $this->get_field_id( 'post' ); ?>">Show Post:</label> 
+			<select id="<?php echo $this->get_field_id( 'post' ); ?>"
+				name="<?php echo $this->get_field_name( 'post' ); ?>"
+				class="widefat" style="width:100%;">
+					<option value="true" <?php if ($post == 'true') echo 'selected="true"'; ?> >Yes</option>
+					<option value="false" <?php if ($post == 'false') echo 'selected="false"'; ?> >No</option>	
+			</select>
+		</p>
+         <p>
+			<label for="<?php echo $this->get_field_id( 'box' ); ?>">Show Box Shadow:</label> 
+			<select id="<?php echo $this->get_field_id( 'box' ); ?>"
+				name="<?php echo $this->get_field_name( 'box' ); ?>"
+				class="widefat" style="width:100%;">
+					<option value="true" <?php if ($box == 'true') echo 'selected="true"'; ?> >Yes</option>
+					<option value="false" <?php if ($box == 'false') echo 'selected="false"'; ?> >No</option>	
+			</select>
+		</p>
+		
+		
+<?php
+    }
+    
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+	    if(empty($title)) $title = "Advanced responsive facebook likebox";
+        if(empty($fb_url)) $fb_url = "https://www.facebook.com/FacebookDevelopers";
+        if(empty($height)) $height = "450";
+        if(empty($backgroundColor)) $backgroundColor = "#ffffff";
+        if(empty($color_scheme)) $color_scheme = "0";
+        if(empty($radius)) $radius = "5";
+        if(empty($header)) $header = "true";
+		 if(empty($face)) $face = "true";
+        if(empty($border)) $border = "true";
+		if(empty($post)) $post = "true";
+        if(empty($box)) $box = "true";
+		
+		
+        echo $before_widget;
+            echo $before_title . $title . $after_title;
+            
+            ?>
+<style>
+.fb_iframe_widget span {
+display: inline-block;
+position: relative;
+text-align: justify;
+width: 100%;
+}
+
+.fb-like-box.fb_iframe_widget {
+    width: 100% !important;
+}
+
+.fb-like-box.fb_iframe_widget span {
+width: 100%;
+}
+.fb-like-box iframe {
+    width: 100% !important;
+}
+
+.fb-like-box span {
+    width: 100% !important;
+}
+</style>
+ <div id="container" style="width:100%; background:<?php echo $backgroundColor;?> ; border-radius:<?php echo $radius;?>px; 
+<?php if($box=='true'){?>
+-webkit-box-shadow: 1px 1px 23px 0px rgba(57, 53, 50, 0.7);
+-moz-box-shadow:    1px 1px 23px 0px rgba(57, 53, 50, 0.7);
+box-shadow:         1px 1px 23px 0px rgba(57, 53, 50, 0.7);
+<?php } else {}?>
+">
+  <div class="fb-like-box" data-href="<?php echo $fb_url;?>" data-colorscheme="light"    data-show-faces="<?php echo $face;?>" data-stream="<?php echo $post;?>" data-header="<?php echo $header;?>" data-show-border="<?php echo $border;?>"></div>
+</div>
+
+<div id="fb-root"></div>
+<script>
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=262562957268319&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+jQuery(window).bind("load resize", function(){    
+  var container_width = $('#container').width();    
+    jQuery('#container').html('<div class="fb-like-box" ' + 
+    'data-href="<?php echo $fb_url;?>"' +
+    ' data-width="' + container_width + '" data-colorscheme="<?php echo $color_scheme;?>" data-height="<?php echo $height;?>" data-show-faces="<?php echo $face;?>" ' +
+    'data-stream="<?php echo $post;?>"  data-header="<?php echo $header;?>" data-show-border="<?php echo $border;?>"></div>');
+    FB.XFBML.parse( );    
+}); 
+</script>
+
+   
+<?php
+        echo $after_widget;
+    }
+}
+//registering the color picker
+function responsive_facebook_likebox_color_picker_script() {
+	wp_enqueue_script('farbtastic');
+}
+function responsive_facebook_likebox_color_picker_style() {
+	wp_enqueue_style('farbtastic');
+}
+add_action('admin_print_scripts-widgets.php', 'responsive_facebook_likebox_color_picker_script');
+add_action('admin_print_styles-widgets.php', 'responsive_facebook_likebox_color_picker_style');
+add_action('widgets_init','register_Responsivefacebook');
+function register_Responsivefacebook(){
+    register_widget('Responsivefacebook');
+}
---
+++ readme.txt
@@ -0,0 +1,72 @@
+=== Advanced Responsive Facebook Likebox ===
+
+Plugin URI: http://www.cr8tech.net
+Author Name : Chris Raven
+Author URL : http://www.cr8tech.net
+Tags: advanced responsive facebook likebox, facebook widget, responsive facebook, facebook likebox, wordpress facebook widget, facebook
+Requires at least: 3.0.1
+Tested up to: 3.9.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Advanced Responsive Facebook Likebox is a very light and useful extension. You can display your facebook likebox which is mobile friendly.
+
+== Description ==
+
+Advanced Responsive Facebook Likebox - Name says might everything. It's just not only responsive but highly editable features can be proved very useful for you.
+
+Some features can be lighten up are:
+
+1. Responsive in Nature. Facebook Likebox + Streams and they are ready for your mobile devices.
+
+2. Facebook Likebox is highly customizable too. You can edit facebook background color of it. Can put border radius. Even started doing some css on it.
+
+3. Everything under control :: yap - You can control over your facebook likebox. Can choose which one to display which one to not.
+Can switch on/off between displaying facebook streams, header, face and even remove border from the facebook likebox :) :)
+
+4. Also the facebook likebox have shadow over it which you can turn on/off too
+
+5. We will love to answer your questions too.
+
+== Installation ==
+
+It's easy to install and configure it. You can install the zip file you have downloaded or using the plugin installer on wordpress.
+
+Once installed you can find our plugins in widget area. And drag and drop the widget to your prefer position - Make sure you have studied your theme widget positions.
+
+Some options for your help:
+
+As we believe the options are quite self-descriptive but we love to help you more so we've tried to describe a little more about these options fields on widget area.
+
+1. Title: Title over the widget positon - if you don't require to display any title just keep it blank.
+
+2. Facebook Page URL: Absolute URL of your facebook page. Note: It doesn't work for facebook profile or either facebook groups not even facebook event. Only Facebook Page URL you need.
+
+3. Height: Height of your module in px.
+
+4. Background Color: Choose your prefer background color. Obviously using your color picker or copy the Hex Color code on your mind (Ok you can copy it too) and put it on the field.
+
+5. Border Radius: Border radius of your module in px.
+
+6. Color Scheme: Choose your preferable color scheme: Light or Dark
+
+7. Show Header: Switch it on/off - Yes for keep it on --- No for keep it off
+
+8. Show Face: Switch it on/off - Yes for keep it on --- No for keep it off
+
+9. Show Post: Switch it on/off - Yes for keep it on --- No for keep it off
+
+10. Show Box Shadow: Switch it on/off - Yes for keep it on --- No for keep it off
+
+== Frequently Asked Questions ==
+
+Support :- sir.chris.raven@gmail.com
+
+== Screenshots ==
+
+1. Facebook Likebox
+
+== Changelog ==
+
+= 1.0 =
+Stable version release
