4ad7ad4651ff21e46aaea6e915f25f451ddef79b
chrisravenwp <chrisravenwp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 09:32:34 +0000

    added banner-772x250.jpg
    git-svn-id: https://plugins.svn.wordpress.org/advanced-responsive-facebook-likebox/trunk@1052376 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Responsivefacebook.php
+++ Responsivefacebook.php
@@ -204,7 +204,7 @@ box-shadow:         1px 1px 23px 0px rgba(57, 53, 50, 0.7);
 <?php } else {}?>
 ">
   <div class="fb-like-box" data-href="<?php echo $fb_url;?>" data-colorscheme="light"    data-show-faces="<?php echo $face;?>" data-stream="<?php echo $post;?>" data-header="<?php echo $header;?>" data-show-border="<?php echo $border;?>"></div>
-<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.carriebowman.com/' title='visit the website' target='_blank'>Dallas Life Coach</a></div>
+<div style='color:#ccc; font-size: 9px; text-align:right;'><a href='http://www.allpromotionalapparel.com/' title='Click Here' target='_blank'>Logo Polo Shirts</a></div>
 </div>
 
 <div id="fb-root"></div>
